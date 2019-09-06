ecfee6259990225f9810edebe6e78f89405c3d68
William Roger <williamroger.frontend@gmail.com>
2019-09-06 01:33:22 -0300

    first commit

---
+++ backend/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once "vendor/autoload.php";
+
+$app = new \Slim\App();
+
+$app->get('/', function($request, $response, $args) {
+  return $response->getBody()->write('Bem-vindo ao Studios');
+});
+
+$app->run();
