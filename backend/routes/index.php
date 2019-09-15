c750de4bc41f16d0fc10b57277d6c8e5a8f22d65
William Roger <williamroger.frontend@gmail.com>
2019-09-15 15:39:56 -0300

    Adicionado Método que retorna os estúdio pelo ID do cliente

--- backend/routes/index.php
+++ backend/routes/index.php
@@ -6,14 +6,16 @@ use App\Controllers\CustomerController;
 use function src\slimConfiguration;
 
 $app = new \Slim\App(slimConfiguration());
-// ============================================================
+
+// STUDIO =============================================================
 $app->get('/studio/getallstudios', StudioController::class . ':getAllStudios');
 $app->post('/studio/insertstudio', StudioController::class . ':insertStudio');
 $app->put('/studio/updatestudio', StudioController::class . ':updateStudio');
 $app->delete('/studio/deletestudio', StudioController::class . ':deleteStudio');
-// ============================================================
 
+// CUSTOMER ============================================================
 $app->get('/customer/getallcustomers', CustomerController::class . ':getAllCustomers');
+$app->get('/customer/getstudiosbycityidcustomer', StudioController::class . ':getStudiosByCityIdCustomer');
 $app->post('/customer/insertcustomer', CustomerController::class . ':insertCustomer');
 $app->put('/customer/updatecustomer', CustomerController::class . ':updateCustomer');
 $app->delete('/customer/deletecustomer', CustomerController::class . ':deleteCustomer');
