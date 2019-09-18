662dc5973c3f6c4769eee9c4e9f98bc23458fa60
William Roger <williamroger.frontend@gmail.com>
2019-09-18 03:21:42 -0300

    Adicionado Model Studio e Classe de Serviço

---
+++ frontend/src/app/pages/register/shared/register.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError} from 'rxjs';
+import { map, catchError, flatMap} from 'rxjs/operators';
+
+import { StudioModel } from './studio.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  private apiPath: string = 'api/register'; //provisório
+
+  constructor(public http: HttpClient) { }
+
+  createStudio(studio: StudioModel): Observable<StudioModel> {
+    return this.http.post(this.apiPath, studio).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToStudio)
+    );
+  }
+
+  /**
+   * Private Methods
+   */
+  private jsonDataToStudio(jsonData: any): StudioModel {
+    return jsonData as StudioModel;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('Erro na Requisição => ', error);
+
+    return throwError(error);
+  }
+}
