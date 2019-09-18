ba02c87a819b7c281c8c077ba8ce145632e34e14
William Roger <williamroger.frontend@gmail.com>
2019-09-18 00:41:05 -0300

    Adicionado CORS Middleware from Tuupola

--- backend/routes/index.php
+++ backend/routes/index.php
@@ -7,22 +7,31 @@ use function src\slimConfiguration;
 
 $app = new \Slim\App(slimConfiguration());
 
-// STUDIO =============================================================
-$app->get('/studio/getallstudios', StudioController::class . ':getAllStudios');
-$app->post('/studio/insertstudio', StudioController::class . ':insertStudio');
-$app->post('/studio/insertroom', StudioController::class . ':insertRoom');
-$app->get('/studio/getallrooms', StudioController::class . ':getAllRooms');
-$app->get('/studio/getroomsbystudioid', StudioController::class . ':getRoomsByStudioId');
-$app->put('/studio/updateroom', StudioController::class . ':updateRoom');
-$app->delete('/studio/deleteroom', StudioController::class . ':deleteRoom');
-$app->put('/studio/updatestudio', StudioController::class . ':updateStudio');
-$app->delete('/studio/deletestudio', StudioController::class . ':deleteStudio');
+$app->group('', function() use ($app) {
+  // STUDIO =============================================================
+  $app->get('/studio/getallstudios', StudioController::class . ':getAllStudios');
+  $app->post('/studio/insertstudio', StudioController::class . ':insertStudio');
+  $app->post('/studio/insertroom', StudioController::class . ':insertRoom');
+  $app->get('/studio/getallrooms', StudioController::class . ':getAllRooms');
+  $app->get('/studio/getroomsbystudioid', StudioController::class . ':getRoomsByStudioId');
+  $app->put('/studio/updateroom', StudioController::class . ':updateRoom');
+  $app->delete('/studio/deleteroom', StudioController::class . ':deleteRoom');
+  $app->put('/studio/updatestudio', StudioController::class . ':updateStudio');
+  $app->delete('/studio/deletestudio', StudioController::class . ':deleteStudio');
 
-// CUSTOMER ============================================================
-$app->get('/customer/getallcustomers', CustomerController::class . ':getAllCustomers');
-$app->get('/customer/getstudiosbycityidcustomer', StudioController::class . ':getStudiosByCityIdCustomer');
-$app->post('/customer/insertcustomer', CustomerController::class . ':insertCustomer');
-$app->put('/customer/updatecustomer', CustomerController::class . ':updateCustomer');
-$app->delete('/customer/deletecustomer', CustomerController::class . ':deleteCustomer');
+  // CUSTOMER ============================================================
+  $app->get('/customer/getallcustomers', CustomerController::class . ':getAllCustomers');
+  $app->get('/customer/getstudiosbycityidcustomer', StudioController::class . ':getStudiosByCityIdCustomer');
+  $app->post('/customer/insertcustomer', CustomerController::class . ':insertCustomer');
+  $app->put('/customer/updatecustomer', CustomerController::class . ':updateCustomer');
+  $app->delete('/customer/deletecustomer', CustomerController::class . ':deleteCustomer');
+})->add(new Tuupola\Middleware\CorsMiddleware([
+  "origin" => ["*"],
+  "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  "headers.allow" => [],
+  "headers.expose" => [],
+  "credentials" => false,
+  "cache" => 0,
+]));
 
 $app->run();
