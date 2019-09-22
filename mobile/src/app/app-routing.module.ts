ca06cf0b6a3888a5ee963b2e47dcda189323cf6b
Gabriel Carvalho <gabrielksi@outlook.com>
2019-09-22 01:57:30 -0300

    Criada tela de login

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: '', loadChildren: './register/register.module#RegisterPageModule' }
+  { path: '', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [
