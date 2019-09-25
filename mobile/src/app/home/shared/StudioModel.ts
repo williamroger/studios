7f359a86755ab95dd151d34256d42918a8585d50
William Luiz <williamlga@gmail.com>
2019-09-24 23:36:56 -0300

    Criado os arquivos homeService e o StudioModel.ts

---
+++ mobile/src/app/home/shared/StudioModel.ts
@@ -0,0 +1,8 @@
+export class StudioModel{
+    public name?: string;
+    public address?: string;
+    public phone?: string;
+    public description?: string;
+    public telephone?: string;
+    public city_id?: number;
+}
