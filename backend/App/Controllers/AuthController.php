cc0c57da92752b51e6e5887ee3cbd82c41790afb
William Roger <williamroger.frontend@gmail.com>
2019-09-27 02:03:15 -0300

    Simplificando o método de Login

--- backend/App/Controllers/AuthController.php
+++ backend/App/Controllers/AuthController.php
@@ -10,7 +10,6 @@ use App\Models\TokenModel;
 use DateTime;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use \Firebase\JWT\JWT;
 
 final class AuthController
 { 
@@ -40,12 +39,10 @@ final class AuthController
       ], 401);
     }
     
-    $expiredAt = (new \DateTime())->modify('+2 days')->format('Y-m-d H:i:s');
-    
     if ($user->getIsCustomer() == 1) {
       $custumer = $customerDAO->getCustomerById(intval($user->getCustomerId()));
-      
-      $tokenPayload = array(
+    
+      $userPayload = array(
         'id' => $user->getId(),
         'customer_id' => $user->getCustomerId(),
         'email' => $user->getEmail(),
@@ -53,44 +50,24 @@ final class AuthController
         'lastname' => $custumer['lastname'],
         'phone' => $custumer['phone'],
         'cpf' => $custumer['cpf'],
-        'city_id' => $custumer['city_id'],
-        'expired_at' => $expiredAt
+        'city_id' => $custumer['city_id']
       );
     } else {
       $studio = $studioDAO->getStudioById(intval($user->getStudioId()));
-     
-      $tokenPayload = array(
+
+      $userPayload = array(
         'id' => $user->getId(),
         'studio_id' => $user->getStudioId(),
         'email' => $user->getEmail(),
         'name' => $studio['name'],
-        'city_id' => $studio['city_id'],
-        'expired_at' => $expiredAt
+        'city_id' => $studio['city_id']
       );
     }
 
-    $token = JWT::encode($tokenPayload, getenv('JWT_SECRET_KEY'));
-
-    $refreshTokenPayload = array(
-      'email' => $user->getEmail()
-    );
-
-    $refreshToken = JWT::encode($refreshTokenPayload, getenv('JWT_SECRET_KEY'));
-    
-    $tokenModel = new TokenModel();
-    $tokenModel->setToken($token)
-    ->setRefreshToken($refreshToken)
-    ->setExpiredAt($expiredAt)
-    ->setUserId($user->getId());
-
-    $tokenDAO = new TokensDAO();
-    $tokenDAO->createToken($tokenModel);
-
     $response = $response->withJson([
       'success' => true,
-      'token' => $token,
-      'refresh_token' => $refreshToken
-    ]);
+      'userPayload' => $userPayload
+    ], 200);
     
     return $response;
   }
