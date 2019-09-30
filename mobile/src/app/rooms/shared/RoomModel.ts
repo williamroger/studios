1f77b7682e95ceded3256baf9a6f3560a324d14b
William Luiz <williamlga@gmail.com>
2019-09-29 21:06:10 -0300

    Feito a page rooms e o roomService

---
+++ mobile/src/app/rooms/shared/RoomModel.ts
@@ -0,0 +1,11 @@
+export class RoomModel {
+    public id?: number;
+    public name?: string;
+    public description?: string;
+    public studio_id: number;
+    public maximum_capacity?: number;
+    public color?: string;
+    //public images?: string;
+    public created_at?: string;
+    public updated_at?: string;
+}
