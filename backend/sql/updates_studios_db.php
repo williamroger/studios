77d90308d6f3817eca3776d72b4ce88390071eab
William Roger <williamroger.frontend@gmail.com>
2019-10-01 15:10:37 -0300

    Update na tabela time_periods

--- backend/sql/updates_studios_db.php
+++ backend/sql/updates_studios_db.php
@@ -11,3 +11,17 @@ CHANGE COLUMN `password` `password` VARCHAR(255) NULL DEFAULT NULL ;
 
 ALTER TABLE `db_dev_studios`.`time_periods`
 CHANGE COLUMN `day` `day` VARCHAR(45) NULL DEFAULT NULL ;
+
+================================================================
+
+ALTER TABLE `db_dev_studios`.`schedules_time_periods`
+DROP FOREIGN KEY `fk_Agendamentos_has_Horarios_Horarios1`,
+DROP FOREIGN KEY `fk_Agendamentos_has_Horarios_Agendamentos`;
+ALTER TABLE `db_dev_studios`.`schedules_time_periods`
+DROP INDEX `fk_Agendamentos_has_Horarios_Agendamentos_idx` ,
+DROP INDEX `fk_Agendamentos_has_Horarios_Horarios1_idx` ;
+
+================================================================
+
+ALTER TABLE `db_dev_studios`.`time_periods`
+CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT ;
