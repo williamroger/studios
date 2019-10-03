b8467636649c288f900d19678c683b8a09053160
AvohaiSilva <avohai.atos@gmail.com>
2019-10-03 01:16:31 -0300

    Criado componente de menu e atualizado dashboard

--- frontend/src/app/pages/dashboard/dashboard.module.ts
+++ frontend/src/app/pages/dashboard/dashboard.module.ts
@@ -1,15 +1,22 @@
+
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HeaderMenuModule } from './../../shared/header-menu/header-menu/header-menu.module';
+import { HeaderMenuComponent } from './../../shared/header-menu/header-menu/header-menu.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    HeaderMenuComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    HeaderMenuModule
   ]
 })
 export class DashboardModule { }
