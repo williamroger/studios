4b48466178f9a3e89861e294eac345a6307b4e77
William Roger <williamroger.frontend@gmail.com>
2019-10-05 19:26:31 -0300

    Método insertSchedule Controller e DAO FUNCIONANDO

--- backend/App/Controllers/ScheduleController.php
+++ backend/App/Controllers/ScheduleController.php
@@ -3,7 +3,9 @@
 namespace App\Controllers;
 
 use App\Controllers\UtilController;
+use App\DAO\SchedulesDAO;
 use App\Models\ScheduleModel;
+use App\Models\SchedulePeriodModel;
 use DateTimeZone;
 use Exception;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -11,12 +13,52 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 final class ScheduleController 
 {
-  public function insertSchedule(Request $request, Response $response, array $args): Response 
+  public function insertSchedule(Request $request, Response $response, array $args) 
   {
     try {
+      $data = $request->getParsedBody();
+      $date = new \DateTime("now", new DateTimeZone('America/Sao_Paulo'));
+      $now = $date->format('Y-m-d H:i:s');
+      $idCustomer = intval($data['customer_id']);
+      $idSchedule = null;
+
+      if (!$idCustomer)
+        throw new Exception('Erro na aplicação, tente novamente.');
       
-    } catch (\Throwable $th) {
-      //throw $th;
+      if (!$data['date_scheduling'] || $data['date_scheduling'] == '')
+        throw new Exception('Você precisa informar a data do agendamento.');
+      
+      if (!$data['time_period_id'] || $data['time_period_id'] == '')
+        throw new Exception('Você precisa escolher um período de ensaio.');
+        
+      $schedule = new ScheduleModel();
+      $schedulePeriod = new SchedulePeriodModel();
+      $scheduleDAO = new SchedulesDAO();
+
+      $schedule->setDateScheduling($data['date_scheduling'])
+      ->setStatus(0)
+      ->setCreatedAt($now)
+      ->setCustomerId($idCustomer)
+      ->setComment($data['comment']);
+
+      $idSchedule = $scheduleDAO->insertSchedule($schedule);
+
+      $schedulePeriod->setScheduleId(intval($idSchedule))
+      ->setTimePeriodId($data['time_period_id']);
+
+      $scheduleDAO->insertScheduleTimePeriod($schedulePeriod);
+
+      $response = $response->withJson([
+        'success' => true,
+        'msg' => 'Ensaio agendado com sucesso!'
+      ], 200);
+
+      return $response;
+    } catch (\Exception $ex) {
+      return $response->withJson([
+        'success' => false,
+        'msg' => $ex->getMessage()
+      ], 500);
     }
   }
 }
--- backend/App/DAO/SchedulesDAO.php
+++ backend/App/DAO/SchedulesDAO.php
@@ -2,6 +2,9 @@
 
 namespace App\DAO;
 
+use App\Models\ScheduleModel;
+use App\Models\SchedulePeriodModel;
+
 class SchedulesDAO extends ConnectionDataBase 
 {
   public function __construct()
@@ -9,5 +12,43 @@ class SchedulesDAO extends ConnectionDataBase
     parent::__construct();
   }
 
-  
+  public function insertSchedule(ScheduleModel $schedule): string 
+  { 
+    $statement = $this->pdo
+      ->prepare('INSERT INTO schedules
+	                  (date_scheduling,
+                     status,
+                     created_at,
+                     customer_id,
+                     comment)
+                 VALUES
+                    (:date_scheduling,
+                     :status,
+                     :created_at,
+                     :customer_id,
+                     :comment);');
+    
+    $statement->execute([
+      'date_scheduling' => $schedule->getDateScheduling(),
+      'status'          => $schedule->getStatus(),
+      'created_at'      => $schedule->getCreatedAt(),
+      'customer_id'     => $schedule->getCustomerId(),
+      'comment'         => $schedule->getComment()
+    ]);
+
+    return $this->pdo->lastInsertId();
+  }
+
+  public function insertScheduleTimePeriod(SchedulePeriodModel $schedulePeriod): void 
+  {
+    $statement = $this->pdo
+      ->prepare('INSERT INTO schedules_time_periods
+                   (schedule_id, time_period_id)
+                 VALUES
+                   (:schedule_id, :time_period_id);');
+    $statement->execute([
+      'schedule_id'    => $schedulePeriod->getScheduleId(),
+      'time_period_id' => $schedulePeriod->getTimePeriodId()
+    ]);
+  }
 }
