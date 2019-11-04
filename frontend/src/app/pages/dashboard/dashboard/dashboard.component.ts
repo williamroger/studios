126a2db51bdc0c348f974b45f9624db05f51ac8e
William Roger <williamroger.frontend@gmail.com>
2019-11-04 17:40:09 -0300

    Adicionado Evento para exibir detalhes dos Agendamentos

--- frontend/src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -20,4 +20,19 @@ export class DashboardComponent implements OnInit {
     this.userLoggedIn = JSON.parse(localStorage.getItem('userLoggedIn'));
     this.studioHasCityId = (+this.userLoggedIn['city_id']) ? true : false;
   }
+
+  private toggleDetails(event) {
+    let target = event.currentTarget;
+    let icon = target.querySelector('.icon');
+    let details = target.closest('.schedule').querySelector('.schedule__body');
+    if (details.classList.contains('d-none')) {
+      details.classList.remove('d-none');
+      icon.classList.remove('pi-chevron-down');
+      icon.classList.add('pi-chevron-up');
+    } else {
+      details.classList.add('d-none');
+      icon.classList.remove('pi-chevron-up');
+      icon.classList.add('pi-chevron-down');
+    }
+  }
 }
